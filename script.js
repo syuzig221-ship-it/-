7d3b59bfa98e76b17f0a17a8d30bbd4684f2377b
syuzig221-ship-it <syuzig221@gmail.com>
2026-04-07 23:32:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -6,11 +6,12 @@ function answer(choice) {
     if(choice === 'yes' || choice === 'love') {
         heart.classList.remove('hidden');
         message.innerText = 'Ես էլ քեզ շատ սիրում, կյանքս 💖';
-        cat.src = 'images/cat_happy.png';
+        cat.src = 'https://www.dreamstime.com/funny-pink-cartoon-cat-green-eyes-illustration-has-big-curly-tail-image226809283';
         cat.style.transform = 'translateY(-20px)';
     } else if(choice === 'no') {
         heart.classList.add('hidden');
         message.innerText = '';
+        cat.src = 'https://www.freepik.com/premium-ai-image/adorable-cartoon-cat-with-big-green-eyes-pink-nose_308859846';
         cat.style.transform = 'translateY(100px)';
         setTimeout(() => {
             cat.style.transform = 'translateY(0)';
